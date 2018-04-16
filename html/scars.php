5854ce83496e2bf6fa130e5420438480b2a570ff
Manoj Sree Harsha Vuppuluri <msharsha1024@gmail.com>
2018-04-16 13:44:39 +0530

    Update scars.php

--- html/scars.php
+++ html/scars.php
@@ -45,7 +45,7 @@ function check2(){
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "1234567";
 $dbname="intern";
 $tablename="cartable";
 echo "<table id=\"myTable\"class=\"abc\"><thead><th class=\"abc\">Id</th><th class=\"abc\">name</th><th class=\"abc\">source</th><th class=\"abc\">price</th><th class=\"abc\">Actions</th></thead><tbody>";
