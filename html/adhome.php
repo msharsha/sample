344739849154b2ba37a83986e81cd6631782de5c
Manoj Sree Harsha Vuppuluri <msharsha1024@gmail.com>
2018-04-16 13:02:37 +0530

    Update adhome.php

--- html/adhome.php
+++ html/adhome.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "1234567";
 $dbname="intern";
 $tablename="cartable";
 echo '<h1 id="fheading">Old Arrivals</h1>';
