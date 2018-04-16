42bea3cad687fb3cf5a60a1529c0a8eaf89e7e98
Manoj Sree Harsha Vuppuluri <msharsha1024@gmail.com>
2018-04-16 13:36:49 +0530

    Update checkemail.php

--- html/checkemail.php
+++ html/checkemail.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "1234567";
 $dbname="intern";
 $tablename="users";
 $conn = new mysqli($servername, $username, $password, $dbname);
